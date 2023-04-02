4d54897291423b7b5149855e8b51c50f175a640a
WilliamWJD <william.jose.wjd@gmail.com>
2023-04-02 11:20:34 -0300

    feat: criando entidades inquilino, endereco, cidade e estado

--- src/main/java/com/sislocacao/api/domain/model/Estado.java
+++ src/main/java/com/sislocacao/api/domain/model/Estado.java
@@ -1,16 +1,9 @@
 package com.sislocacao.api.domain.model;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.sislocacao.api.domain.enumerators.EstadoCivilEnum;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,22 +11,12 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario {
-	
+public class Estado {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
-	
+
 	private String nome;
-	private String rg;
-	private String cpf;
-	private String nacionalidade;
-	private String email;
-	private String senha;
-	
-	private EstadoCivilEnum estadoCivil;
-	
-	@OneToMany
-	private List<Endereco> enderecos = new ArrayList<>();
 }
--- src/main/java/com/sislocacao/api/domain/model/Inquilino.java
+++ src/main/java/com/sislocacao/api/domain/model/Inquilino.java
@@ -1,14 +1,10 @@
 package com.sislocacao.api.domain.model;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import com.sislocacao.api.domain.enumerators.EstadoCivilEnum;
 
@@ -18,7 +14,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario {
+public class Inquilino {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,11 +25,6 @@ public class Usuario {
 	private String rg;
 	private String cpf;
 	private String nacionalidade;
-	private String email;
-	private String senha;
-	
 	private EstadoCivilEnum estadoCivil;
-	
-	@OneToMany
-	private List<Endereco> enderecos = new ArrayList<>();
+	private String profissao;
 }
